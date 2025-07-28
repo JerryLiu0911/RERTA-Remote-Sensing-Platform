fc6022dcc9339ef2b200a2b9ed30d48bd4bfff4e
JerryLiu0911 <89786918+JerryLiu0911@users.noreply.github.com>
2025-07-28 17:30:08 +0100

    Sync test with Google Colab

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ def main():
     BC_df = merged_df[merged_df['point.label'].str.contains("BC", case=False, na=False)]
     plot_relations(BC_df,'average_canopy_openness', '_mean_CHM', geo =True)
 
+
+    print("sync testtt")
     #statistical_modelling.random_forest_regression(merged_df, 'average_canopy_openness', [feature for feature in merged_df.columns if feature not in ['geometry', 'point.label', 'average_canopy_openness']])
     statistical_modelling.multi_linear_regression_display(merged_df, 'average_canopy_openness', [column for column in merged_df.columns if'CHM' in column], display=False)
     #statistical_modelling.multi_linear_regression_display(merged_df, 'average_canopy_openness', [column for column in merged_df.columns if column not in ['geometry', 'point.label', 'average_canopy_openness']], display=False)
