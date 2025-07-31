4a9ccc5e019c1a3634c5d588e6275029df9bb174
Jerry Liu <89786918+JerryLiu0911@users.noreply.github.com>
2025-07-31 18:18:28 +0100

    Updated CHM zonal statistics with filtered 2019 data, needs fixing.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import align_coords
 import coordinate_extraction
 import statistical_modelling
+import gis as gis
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,6 +10,7 @@ def plot_relations(df, target, column, geo = False):
     """
     Plots average canopy openness against CHM for the BC points.
     """
+    print("HIYAM THIS IS" ,df[column])
     if geo:
         plt.scatter(df[target], df['point.label']
                     , label=target
@@ -31,20 +33,12 @@ def plot_relations(df, target, column, geo = False):
         plt.legend()
         plt.show()
 
-
-def main():
-    paths = {
-    'canopy_openness': "Data/3.4-canopy.openness.csv",
-    'coordinates': "Data/Rerta koordinate 2018_09_24.gpkg",
-    'GLI': "Data/Palapa June2019 GLI statistics.gpkg",
-    'ExG': "Data/Palapa June2019 ExG statistics.gpkg",
-    'DEM': "Data/Palapa June2019 DEM statistics.gpkg",
-    'CHM': "Data/Palapa June2019 CHM statistics.gpkg"
-    }
+def main(paths):
 
     coordinate_extraction.extract_coords(paths['coordinates'])
     align_coords.canopy_openness(paths['canopy_openness'],timepoint=2019)
-    
+    # gis.zonal_statistics("canopy_openness_result.gpkg", "G:/My Drive/UROP/UROP Rerta Palapa June2019 CHM.tif", "buffered_points.gpkg", paths['CHM'])
+
     merged_df = statistical_modelling.load_data(
             [('GLI', paths['GLI']),
             ('ExG', paths['ExG']),
@@ -52,10 +46,21 @@ def main():
             ('CHM', paths['CHM'])])
 
     BC_df = merged_df[merged_df['point.label'].str.contains("BC", case=False, na=False)]
-    plot_relations(BC_df,'average_canopy_openness', '_mean_CHM', geo =True)
+    print(BC_df.head())
+    plot_relations(BC_df,'average_canopy_openness', 'mean_CHM', geo =True)
 
     #statistical_modelling.random_forest_regression(merged_df, 'average_canopy_openness', [feature for feature in merged_df.columns if feature not in ['geometry', 'point.label', 'average_canopy_openness']])
-    statistical_modelling.multi_linear_regression_display(merged_df, 'average_canopy_openness', [column for column in merged_df.columns if'CHM' in column], display=False)
+    # statistical_modelling.random_forest_ensemble(merged_df, 'average_canopy_openness', [feature for feature in merged_df.columns if feature not in ['geometry', 'point.label', 'average_canopy_openness']])
+    #statistical_modelling.multi_linear_regression_display(merged_df, 'average_canopy_openness', [column for column in merged_df.columns if'CHM' in column], display=False)
     #statistical_modelling.multi_linear_regression_display(merged_df, 'average_canopy_openness', [column for column in merged_df.columns if column not in ['geometry', 'point.label', 'average_canopy_openness']], display=False)
 
-main()
+
+paths = {
+'canopy_openness': "Data/3.4-canopy.openness.csv",
+'coordinates': "Data/Rerta koordinate 2018_09_24.gpkg",
+'GLI': "Data/Palapa June2019 GLI statistics.gpkg",
+'ExG': "Data/Palapa June2019 ExG statistics.gpkg",
+'DEM': "Data/Palapa June2019 DEM statistics.gpkg",
+'CHM': "Data/Palapa June2019 CHM statistics.gpkg"
+}
+main(paths)
